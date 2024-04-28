376b82df34593b75de75b2a37ee0692e7d54981d
AnonyMOOOOusGuy <168319866+AnonyMOOOOusGuy@users.noreply.github.com>
2024-04-28 19:27:21 +0800

    Update generate_decoy.py

--- code/generate_decoy.py
+++ code/generate_decoy.py
@@ -5,6 +5,5 @@ import utils
 def generate_decoy_files(main_model, weight_model, user_vec_dict):
 
     # pick certain attrs that decoy files should have
-    # take file attribute size_rank for example:
     # ... ...
 
